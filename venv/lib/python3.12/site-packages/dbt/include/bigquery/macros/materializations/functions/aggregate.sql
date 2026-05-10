431719e282a01924093b1c0058afc51f6a4b1101
Walid Bouhedda <walidbouhedda73@gmail.com>
2026-05-10 14:42:58 +0100

    remove venv, add gitignore

--- venv/lib/python3.12/site-packages/dbt/include/bigquery/macros/materializations/functions/aggregate.sql
+++
@@ -1,5 +0,0 @@
-{% macro bigquery__aggregate_function_sql(target_relation) %}
-    {{ get_aggregate_function_create_replace_signature(target_relation) }}
-    {# conveniently we can reuse the sql scalar function body #}
-    {{ scalar_function_body_sql() }}
-{% endmacro %}
